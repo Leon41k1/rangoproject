8982c066c5aabd27fd66fa725cb43817e8277490
Leo Ihnatiuk <3015039I@student.gla.ac.uk>
2026-02-10 23:34:20 +0000

    Finished chap 3

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+# this part is allowing to add indexes to a project
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("How to build ur masles")
